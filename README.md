19d438d3d539607c32ef148c502310882460872e
Malaq88 <atrito12@gmail.com>
2021-02-08 23:37:57 -0300

    documentação e finalização do projeto

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
 # prescriptions_backend
+
+### Solução
+Implementada uma API REST, contendo o endpoint `[POST] /prescriptions`, para inserir novas prescrições.
+
+ - Não cofigurei nenhum banco de dados relacional, foi feito em SQLite;
+ - Os [serviços dependentes] São consultados antes de salvar alguma prescrição, validando se ele existe no serviço, se o serviço está disponível tentando até 5 conexões antes de desistir, tem um tempo de timeout de 10 segundo de tolerancia para a resposta do serviço;
+ - Se o serviço de clínicas não é validado, porém é selecionada um id padrão para compor o banco(decidi que seria melhor asism);
+ - Os dados deverão são integrados com o serviço de métricas, caso não eles não existam no serviço não é salvo no banco e é retornada mensagem de erro como listado no desafio;
+ - A API REST deverá retorna um erro quando exceder o timeout de 10 segundos ou a quantidade de tentativas de 5 vezes de algum serviço dependente;
+
+O request para inserção é o abaixo
+
+*Request*
+```bash
+curl -X POST   http://localhost:8000/prescriptions/  \
+ -H 'Content-Type: application/json'  \
+  -d '{
+    "clinic": '1',
+    "physician": '1',
+    "patient": '',
+    "text": "1"
+}'
+```
+
+*Response.body*
+```json
+
+{
+   "clinic":{
+      "id":1
+   },
+   "physician":{
+      "id":1
+   },
+   "patient":{
+      "id":1
+   },
+   "text":"Dipirona 1x ao dia"
+}
+```
+
+
+### O que é necessário para rodar o projeto localmente:
+ - Ter pyton 3 instado;
+ - ter pyp3 instalado;
+ - para instalar as dependencia utilizar o comando abaixo;
+    - ``` pip3 install -r requirements.txt ``` 
+ - criar o banco de dados com os comando abaixo;
+    - ``` 
+        python3 manage.py makemigrations 
+        python3 manage.py migrate
+
+      ```
+ - para rodar os testes unitários;
+    - ``` python3 manage.py test ``` 
+ - Princípios SOLID;
+
+
+ Infelizmente eu não tinha certos conhecimentos de utilização de serviços externos ao Django Rest Fremework, por isso procurei algumas bibliotecas do Python para acessar o serviço e fazer a validações solicitadas, também não consegui simular os timeouts ou queda de serviços, por isso não consegui incluílos nos testes.
+
+Acredito que violei algumas best pratices do django ao validar diretamente no controller, aconteceu porque eu não tive muito tempo de refatorar o código. 
+
+Queria muito ter feito deploy em um container Docket, porém ainda não fiz utilização e não fiquei 100% confiante de faze-lo por isso também não perdi muito tempo com essa parte.
+
+___
+Muito obrigado pela oportunidade e principalmente por aumentar ainda mais meu conhecimento, com esse desafio eu pensei em algumas aplicações nas quais tenho ideias porém ainda não sabia como executa-las,
+
+
+Antonio Celso Filho.
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,60 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.core.checks import messages
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from .models import Prescription
+
+class PostTestCase(APITestCase):
+    
+    def test_lis_all_prescriptions(self):
+        url = reverse('prescription-list')
+
+        prescription_1 = Prescription.objects.create(clinic=1, physician=1, patient=1, text= "teste de post")
+        prescription_2 = Prescription.objects.create(clinic=2, physician=2, patient=2, text= "teste de post 2")
+        prescription_3 = Prescription.objects.create(clinic=3, physician=3, patient=3, text= "teste de post 3")
+
+        response = self.client.get(url, {}, format='json')
+        
+        self.assertEqual(response.json()[0]['clinic'], prescription_1.clinic)
+        self.assertEqual(response.json()[1]['clinic'], prescription_2.clinic)
+        self.assertEqual(response.json()[2]['clinic'], prescription_3.clinic)
+
+        self.assertEqual(response.json()[0]['physician'], prescription_1.physician)
+        self.assertEqual(response.json()[1]['physician'], prescription_2.physician)
+        self.assertEqual(response.json()[2]['physician'], prescription_3.physician)
+
+        self.assertEqual(response.json()[0]['patient'], prescription_1.patient)
+        self.assertEqual(response.json()[1]['patient'], prescription_2.patient)
+        self.assertEqual(response.json()[2]['patient'], prescription_3.patient)
+
+        self.assertEqual(response.json()[0]['text'], prescription_1.text)
+        self.assertEqual(response.json()[1]['text'], prescription_2.text)
+        self.assertEqual(response.json()[2]['text'], prescription_3.text)
+        
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    
+    def test_insert_prescription(self):
+        url = 'http://localhost:8000/prescriptions/'
+        response = self.client.post(url, {'clinic': '1', 'physician':'1', 'patient': '1', 'text': "teste de post"}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_physician_not_found(self):
+        url = 'http://localhost:8000/prescriptions/'
+        response = self.client.post(url, {'clinic': '1', 'physician':'999', 'patient': '1', 'text': "teste de post"}, format='json')
+
+        self.assertEqual(response.json()['error']['message'], 'physician not found')
+        self.assertEqual(response.json()['error']['code'], '02')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_patient_not_found(self):
+        url = 'http://localhost:8000/prescriptions/'
+        response = self.client.post(url, {'clinic': '1', 'physician':'1', 'patient': '999', 'text': "teste de post"}, format='json')
+
+        self.assertEqual(response.json()['error']['message'], 'patient not found')
+        self.assertEqual(response.json()['error']['code'], '03')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
